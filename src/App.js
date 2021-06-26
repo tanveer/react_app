987dfbb76bada930571336ed8921248a284c1536
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-06-26 15:39:17 -0400

    Initila working smaple with data fetch from mock api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import { useState, useEffect} from 'react'
+import { Card, CardDeck, CardColumns, Container, Row, Col} from "react-bootstrap"
 import './App.css';
 
-function App() {
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts"
+const USERS_URL = "https://jsonplaceholder.typicode.com/users"
+
+function Post({post}) {
+  const {userId, id, title, body} = post
+  const [userInfo, setUserInfo] = useState([])
+  useEffect(() => {
+    getUserInfo()
+  })
+
+  const getUserInfo = () => {
+    fetch(`${USERS_URL}/${userId}`)
+    .then(res => res.json())
+    .then(setUserInfo)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Card bg="info" text="black" style={{ width: "25rem", height: "15rem" }}>
+        <Card.Body>
+          <Card.Title style={{fontSize: "1.25rem", fontWeight: "Bold", textTransform: "capitalize", color: "white"}}>{title}</Card.Title>
+          <Card.Text style={{fontSize: "1rem", color: "black"}}>
+            {body}
+          </Card.Text>
+          <Card.Subtitle>{userInfo.name}</Card.Subtitle>
+        </Card.Body>
+      </Card>
+  ) 
+}
+
+function App() {
+  const  [posts, setPosts] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    getUsers()
+    getPosts()
+  })
+
+  const getUsers = () => {
+    fetch(USERS_URL)
+    .then(res => res.json())
+    .then(setUsers)
+    .catch(console.log("Failed"))
+  }
+
+  const getPosts = (id) => {
+    fetch(POSTS_URL)
+    .then(res => res.json())
+    .then(setPosts)
+    .catch(console.log("Failed"))
+  }
+
+  return (
+    <Container>
+      <Row>
+      {
+        posts.map ( post => 
+        <Col className="mt-4" >
+          <Post post={post}/> 
+        </Col>
+        )
+      }
+      </Row>
+   </Container>
+
   );
 }
 
 export default App;
+
+
+
