f4dd787c80b82a923ee7cde5f906455a0cd20b11
Tanveer Bashir <tan.m.bashir@gmail.com>
2021-06-26 23:23:41 -0400

    add detail and user posts with routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
 import { useState, useEffect} from 'react'
-import { Card, CardDeck, CardColumns, Container, Row, Col} from "react-bootstrap"
+import { Card, Container, Row, Col} from "react-bootstrap"
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const POSTS_URL = "https://jsonplaceholder.typicode.com/posts"
 const USERS_URL = "https://jsonplaceholder.typicode.com/users"
 
+function UserProfile({user}) {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    getUserPosts()
+  })
+  
+  const getUserPosts = () => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
+      .then(res => res.json())
+      .then(setPosts);
+  }
+
+  return (
+    <Container>
+      <h1>Posts by, {user.name}</h1>
+      <Row>
+          {
+            posts.map ( post => 
+            <Col className="mt-4" >
+                <Post post={post} /> 
+            </Col>
+            )
+          }
+      </Row> 
+    </Container>
+  )
+}
+
+
+function Comments({id}) {
+  const comments_Url = `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    getCommments()
+  })
+
+  const getCommments = () => {
+    fetch(comments_Url)
+    .then(res => res.json())
+    .then(setComments)
+  }
+
+  return (
+    <>
+
+      {
+        comments.map( c => 
+          <div key={c.id}>
+              <hr></hr> <p>{c.body}</p>
+          </div>  
+        )
+      }
+    </>
+  )
+}
+
 function Post({post}) {
   const {userId, id, title, body} = post
   const [userInfo, setUserInfo] = useState([])
+  
   useEffect(() => {
     getUserInfo()
   })
@@ -21,14 +86,40 @@ function Post({post}) {
   return (
       <Card bg="info" text="black" style={{ width: "25rem", height: "15rem" }}>
         <Card.Body>
-          <Card.Title style={{fontSize: "1.25rem", fontWeight: "Bold", textTransform: "capitalize", color: "white"}}>{title}</Card.Title>
-          <Card.Text style={{fontSize: "1rem", color: "black"}}>
-            {body}
-          </Card.Text>
-          <Card.Subtitle>{userInfo.name}</Card.Subtitle>
+           <Card.Title style={{fontSize: "1.25rem", fontWeight: "Bold", textTransform: "capitalize", color: "white"}}>{title}</Card.Title>
+              <Link to={`/${id}`}>
+              <Card.Text style={{fontSize: "1rem", color: "black"}}>
+              {body}
+              </Card.Text>
+              </Link>
+            <Card.Subtitle>{userInfo.name}</Card.Subtitle>
         </Card.Body>
       </Card>
   ) 
+}
+
+function Detail({post, userInfo}) {
+  const {id, title, body} = post
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card text="black" style={{ width: "auto", height: "auto" }}>
+            <Card.Body>
+              <Card.Title style={{fontSize: "1.25rem", fontWeight: "Bold", textTransform: "capitalize"}}>{title}</Card.Title>
+              <Card.Text style={{fontSize: "1rem", color: "black"}}>
+                {body}
+              </Card.Text>
+              <Link to={`/user/${id}`}>
+                 <Card.Subtitle>{userInfo.name}</Card.Subtitle>
+              </Link>
+              <Comments id={id}/>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 function App() {
@@ -55,19 +146,59 @@ function App() {
   }
 
   return (
-    <Container>
-      <Row>
-      {
-        posts.map ( post => 
-        <Col className="mt-4" >
-          <Post post={post}/> 
-        </Col>
-        )
-      }
-      </Row>
-   </Container>
+    <Router>
+     <Switch>
+        <Container>
+            <Row>
+              {
+                posts.map ( post => 
+                <Col className="mt-4" >
+                    <Route exact={true} path="/">
+                        <Post post={post} /> 
+                    </Route>
+                </Col>
+                )
+              }
+            </Row> 
+          <Route 
+            exact
+            path="/:postId"
+            render={ props => {
+              let selectedPost = posts.find(
+                post =>
+                  parseInt(post.id) === parseInt(props.match.params.postId)
+              )
 
-  );
+              let selectedUser = users.find(
+                user => parseInt(user.id) === parseInt(selectedPost.userId)
+              )
+
+              return selectedPost !== undefined ? (
+                  <Detail post={selectedPost} userInfo={selectedUser}/>
+              ) : (
+                <h1>Nothing here!!</h1>
+              )
+            }}
+          />
+          <Route
+            exact
+            path="/user/:userId"
+            render={props => {
+              let selectedUser = users.find(
+                user =>
+                  parseInt(user.id) === parseInt(props.match.params.userId)
+              );
+              return selectedUser !== undefined ? (
+                <UserProfile user={selectedUser} />
+              ) : (
+                <h1>Nothing here!!</h1>
+              )
+            }}
+          />      
+        </Container>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
